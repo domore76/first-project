aca3fc7c6ecfaec236ef73642c1e40ceddd079a1
Anton Kiryanov <kiryanovproduction@gmail.com>
2020-07-18 16:11:33 +0300

    BLL(DATA)-UI. We take the data to the side

--- my-network/src/components/Dialogs/Dialogs.jsx
+++ my-network/src/components/Dialogs/Dialogs.jsx
@@ -19,26 +19,40 @@ const MessageItem = (props) => {
     )
 }
 
+    let dialogsData = [
+        {id: 1, name: "Nikita"},
+        {id: 2, name: "Masha"},
+        {id: 3, name: "Petr"},
+        {id: 4, name: "Vova"},
+        {id: 5, name: "Mitya"}
+    ];
+
+    let messageData = [
+        {id: 1, text: "Hi, what is you name?"},
+        {id: 2, text: "How are your it-kamasutra?"},
+        {id: 3, text: "Yo"},
+    ]
+
+
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
 
-                <DialogItem name="Nikita" id="1"/>
-                <DialogItem name="Masha" id="2"/>
-                <DialogItem name="Petr" id="3"/>
-                <DialogItem name="Vova" id="4"/>
-                <DialogItem name="Mitya" id="5"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+
 
             </div>
             <div className={s.messages}>
 
-                <MessageItem text="Hi, what is you name?"/>
-                <MessageItem text="How are your it-kamasutra?"/>
-                <MessageItem text="Yo"/>
-                <MessageItem text="Yo"/>
-                <MessageItem text="Yo"/>
-
+                <MessageItem text={messageData[0].text} id={messageData[0].id}/>
+                <MessageItem text={messageData[1].text} id={messageData[1].id}/>
+                <MessageItem text={messageData[2].text} id={messageData[2].id}/>
             </div>
         </div>
     );
--- my-network/src/components/Profile/MyPosts/MyPost.jsx
+++ my-network/src/components/Profile/MyPosts/MyPost.jsx
@@ -3,6 +3,14 @@ import s from './MyPost.module.css';
 import Post from "./Post/Post"
 
 const MyPost = () => {
+
+    let postData = [
+        {id: 1, text: "Hi, what is you name?", likeCounts: 12},
+        {id: 2, text: "Solo learning React!", likeCounts: 98},
+        {id: 3, text: "Hi, what is you name?", likeCounts: 3},
+        {id: 4, text: "Hi, what is you name?", likeCounts: 9},
+    ]
+
     return (
         <div className={s.content}>
             My posts
@@ -16,8 +24,10 @@ const MyPost = () => {
             </div>
 
             <div className={s.posts}>
-                <Post message='Hello, World!' likeCounts='15 '/>
-                <Post message='Solo learning React!' likeCounts='20 '/>
+                <Post message={postData[0].text} likeCounts={postData[0].likeCounts}/>
+                <Post message={postData[1].text} likeCounts={postData[1].likeCounts}/>
+                <Post message={postData[2].text} likeCounts={postData[2].likeCounts}/>
+                <Post message={postData[3].text} likeCounts={postData[3].likeCounts}/>
             </div>
         </div>
     );
